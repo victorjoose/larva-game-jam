9ffa29b76d5bd7af0185074d6dd93a7a6746c3de
Jose Farias <josevictorjop@gmail.com>
2022-10-17 02:03:37 -0300

    Reorganizando, adicionado audio

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import { getDogImg, getCatImg, getRandomImg } from "../test/getImage.js";
+import { getDogImg, getCatImg, getRandomImg } from "./getImage.js";
 
 const square1 = document.getElementById('img1');
 const square2 = document.getElementById('img2');
@@ -88,66 +88,8 @@ nextTest.addEventListener('click', () => {
     mark = -1
 })
 
-let selectedSquare = 0;
-let score = 0;
-selectedSquare = getSelectedSquare();
-verifySelection(selectedSquare)
-
-
-
-function getSelectedSquare() {
-    // square1.addEventListener('click', () => {
-    //     return 1;
-    // });
-    
-    // square2.addEventListener('click', () => {
-    //     return 2;
-    // });
-    
-    // square3.addEventListener('click', () => {
-    //     return 3;
-    // });
-    
-    // square4.addEventListener('click', () => {
-    //     return 4;
-    // });
-}
-
-function correctAnswer() {
-    //escolhe uma imagem para ser a imagem certa
-}
-
-function verifySelection(selectedSquare, correctAnswer) {
-    //verifica se a seleção está correta
-    if (correctAnswer == selectedSquare) {
-        score++;
-        next()
-    } else {
-        gameOver()
-    }
-}
-
-function next() {
-
-}
-
-function hardener() {
-    // deixa o jogo mais dificil com o tempo 
-    // adiciona blur
-    // greyscale
-}
-
-function gameOver() {
-    // Tela de game over
-}
-
-function restart() {  
-    // recomeça o jogo
-}
-
 function start(){
     setEscolha()
 }
-
 
 start()
